7a1f94316d6f34137112e2b92b05aa7f690cd54c
sravya <vsry0897@msitprogram.net>
2020-04-14 08:24:28 +0530

    Lru-cache

--- LRUCacheProblem.py
+++ LRUCacheProblem.py
@@ -5,9 +5,29 @@ class LRUCacheProblem:
         self.cache={}
         self.lru=[]
 
-    def put(self,key,value):
-        pass
-    def get(self,key):    
-        pass
+    def get(self,key):
+        if key in self.lru:
+            return self.cache[key]
+        return None 
+    def put(self,key):
+        if len(self.lru) < self.capacity:
+            if key in self.lru:
+                self.lru.remove(key)
+                self.lru.append(key)  
+                self.cache[key]  = key
+            else:
+                self.lru.append(key)
+                self.cache[key] = key
+        else:
+            
+            r = self.lru.pop(0)
+            self.lru.append(key)
+            del self.cache[r]
+            self.cache[key] = key
+            
+            
     def get_cache(self):
-        pass    
+        lists = []
+        for key in reversed(self.lru):
+            lists.append(self.cache[key])
+        return lists 
